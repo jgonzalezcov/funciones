a0b513ff0aa7cf91448a3b036e3e3b8434a3d903
Juan Gonzalez Covarrubias <104181238+jgonzalezcov@users.noreply.github.com>
2022-07-12 21:41:09 -0400

    feat:The js and html file from point 3 is created

---
+++ assets/js/3_script.js
@@ -0,0 +1,11 @@
+/*************************************************************************************
+ *Requerimiento 3: Este JS interacual con el archivo html "3_pintar.html"             *
+ *************************************************************************************/
+
+const pintar = function (ele,color) {
+  ele.style.backgroundColor = color
+}
+
+const element = document.getElementById('ele1')
+element.setAttribute('style', 'color:green')
+element.addEventListener('click', () => pintar(element,'yellow'))
