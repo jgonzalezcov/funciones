54dd35dbcd1f1ca18146c1e8c02f12ada7e17086
Juan Gonzalez Covarrubias <104181238+jgonzalezcov@users.noreply.github.com>
2022-07-13 22:28:34 -0400

    fix:Changes are made in the 3_script.js file, in which the color was left at the beginning with green color and not the backgroundcolor

--- assets/js/3_script.js
+++ assets/js/3_script.js
@@ -2,10 +2,11 @@
  *Requerimiento 3: Este JS interactúa con el archivo html "3_pintar.html"             *
  *************************************************************************************/
 
-const pintar = function (ele,color) {
-  ele.style.backgroundColor = color
+const pintar = function (ele, color) {
+  ele.setAttribute('style', 'background-color:' + color)
 }
 
 const element = document.getElementById('ele1')
-element.setAttribute('style', 'color:green')
-element.addEventListener('click', () => pintar(element,'yellow'))
+element.setAttribute('style', 'background-color:green')
+
+element.addEventListener('click', () => pintar(element, 'yellow'))
